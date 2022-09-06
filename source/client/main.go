39cc27ef2ed1c7ce54631b96702f8a30c5cd59eb
weiqiangxu <weiqiangxu@dofun.cn>
2022-09-06 15:07:24 +0800

    add message

--- source/client/main.go
+++ source/client/main.go
@@ -23,17 +23,22 @@ var (
 
 func main() {
 	flag.Parse()
+
 	// 设置与服务器的连接
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+
+	// 建立 Client
 	c := pb.NewGreeterClient(conn)
 
 	// 连接服务器并打印出它的响应
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
+	// 调用 client.SayHello
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
